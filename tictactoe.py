86b3c0fa865bef3507ff2000b5c5562538c4c849
nontharatbang <73401553+nontharatbang@users.noreply.github.com>
2022-12-15 16:18:53 +0700

    update bot move

--- tictactoe.py
+++ tictactoe.py
@@ -1,12 +1,16 @@
+from pyswip import Prolog
+
 class TicTacToe:
     def __init__(self):
         self.board = []
         self.player_turn = True
-        self.player_value = "X"
+        self.player_value = "o"
+        self.prolog = Prolog()
+        self.prolog.consult('minimax.pl')
 
     def create_board(self):
         for i in range(9):
-            self.board.append('-')
+            self.board.append('n')
 
     def draw_board(self):
         board_len = len(self.board)
@@ -21,9 +25,12 @@ class TicTacToe:
 
     def check_win(self, player):
         win = None
-        value = 'X' if player == "player" else 'O'
+        value = 'o' if player == "player" else 'x'
         board_pos = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
         board_len = len(board_pos)
+
+        if self.board_full():
+            return False
 
         #check winning rows
         for i in range(board_len):
@@ -69,14 +76,15 @@ class TicTacToe:
             return win
 
         for val in self.board:
-            if val == '-':
+            if val == 'n':
                 return False
+
         print("win default")
         return True
 
     def board_full(self):
         for val in self.board:
-            if val == '-':
+            if val == 'n':
                 return False
         return True
 
@@ -84,7 +92,11 @@ class TicTacToe:
         return "com" if player == "player" else "player"
     
     def is_filled(self, pos):
-        return True if self.board[pos] != '-' else False
+        return True if self.board[pos] != 'n' else False
+
+    def bot_move(self, new_board):
+        for i in range(len(new_board)):
+            self.board[i] = str(new_board[i])
 
     def play(self):
         self.create_board()
@@ -115,15 +127,13 @@ class TicTacToe:
                 
                 self.draw_board()
                 #com function minimax
-                pos = int(input("Input: "))
-                if self.is_filled(pos):
-                    print("The position is already filled. Choose a new one.")
-                    continue
-                self.board[pos] = 'O'
+                # pos = sorted(self.prolog.query("minimax({}, Pos".format(self.board)))
+                new_val = sorted(self.prolog.query("minimax({}, Pos)".format(self.board)))[0]["Pos"]
+                self.bot_move(new_val)
                 
                 #check win condition
                 if self.check_win("com"):
-                    print("Com's win!")
+                    print("Bot's win!")
                     break
             
             if self.board_full():
